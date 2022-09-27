d614987f64d84905e3cb04d200b3ef01ea42175f
Trieu Huynh <vikingtc4@gmail.com>
2022-09-27 06:24:56 -0400

    Update code for 001_exercise

--- 001_exercise.pl
+++ 001_exercise.pl
@@ -5,17 +5,17 @@ $count = 0;
 
 # Part a
 $lengthOfArray = @arr;
-print "=== Part a ===\n";
+print "a/ Print length, the first and last element of array\n";
 print "Length of Array: $lengthOfArray\n";
 print "First Element: @arr[0]\n";
 print "Last Element: @arr[$lengthOfArray - 1]\n";
 
 # Part b
-print "=== Part b ===\n";
+print "b/ Print all elements of array\n";
 print "All elements: @arr\n";
 
 # Part c
-print "=== Part c ===\n";
+print "c/ Verify if any element in array contains 'tables'\n";
 for($i = 0; $i < $lengthOfArray ; $i = $i+1){
 	if($arr[$i] eq "tables"){
 		print "Index of elements in array contains 'tables': $i\n";
